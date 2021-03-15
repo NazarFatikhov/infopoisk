605409e8fd393f25ba5195ee587dcba11299646c
Nazar Fatikhov <nazarfatichov@mail.ru>
2021-03-15 19:01:59 +0300

    add utils

---
+++ utils.py
@@ -0,0 +1,10 @@
+def camel_case_split(str):
+    words = [[str[0]]]
+
+    for c in str[1:]:
+        if words[-1][-1].islower() and c.isupper():
+            words.append(list(c))
+        else:
+            words[-1].append(c)
+
+    return [''.join(word) for word in words]
